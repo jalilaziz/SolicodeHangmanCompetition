7ca86a6fa04cffd17ce93ff61c711d8022ccfc8d
AyoubyAmazu <ayoubamazu@gmail.com>
2023-12-12 22:27:23 +0100

    new js by me

--- script.js
+++ script.js
@@ -2,59 +2,72 @@
 fetch('hangman-game.json')
     .then((response) => response.json()) // Parse the response as JSON
     .then(async (json) => {
-        // Check if the current page is Hangman-Game.html
-        if (window.location.pathname.includes("Hangman-Game.html")) {
+
+     
 
             // Extract 'id' parameter from the URL query string using URLSearchParams
             const params = new URLSearchParams(window.location.search);
             const id = params.get('id');
 
             // Generate a random index 'i' to select a question from the specified category
-            let i = Math.floor(Math.random() * 7);
+            let i = Math.floor(Math.random() * 6);
 
-            // let para = new URLSearchParams(window.location.search);
-            // let id = para.get("id");
-            // let questionData = json[id][i].question
-            // let answerData = json[id][i].answer.split('')
+            // Retrieve question and answer data for the selected category and question
+            let questionData = json[id][i].question;
+            let answerData = json[id][i].answer.replaceAll(" ","").split('');
 
-            // let question = document.getElementById("id")
-            // question.innerHTML = questionData
-
-            // let answer = document.getElementById("answer")
-
-            // for(let i =0 ; i< answerData.length;i++){
-            //     let div = document.createElement("div")
-            //     div.classList.add("")
-            //     div.setAttribute("id",answer[i])
-            //     answer.appendChild(div)
-            // }
-
-            // Access the data for the selected category and question
-            let category_data = json[id][i];
-
-            // Display the question on the HTML page
+            // Find the HTML elements to update with the question and answer data
             let question = document.getElementById("question");
-            question.innerHTML = category_data.question;
+            question.innerHTML = questionData;
 
-            // Get the answer for the selected question
-            let answer = category_data.answer;
+            let answer = document.getElementById("answer");
 
-            // Split the answer string into an array of characters using ', ' as the delimiter
-            let answerArray = answer.split(', ');
-            
+            // Create div elements for each character in the answerData
+            for (let j = 0; j < answerData.length; j++) {
+                let div = document.createElement("div");
+                div.classList.add("answer_place");
+                div.setAttribute("id", j);
+                answer.appendChild(div);
+            }
 
-        }
+            // Define the alphabet for keyboard buttons
+            let alphabet = "qwertyuiopasdfghjklzxcvbnm";
 
-        //2
+            //for validing if u lose or won 
+            let x = 0;
+            let keyboardDiv = document.getElementById("keyboardDiv")
 
-//3
+            // Create keyboard buttons dynamically
+            for (let k = 0; k < alphabet.length; k++) {
+                let keyboard = document.createElement("button");
+                keyboardDiv.appendChild(keyboard)
+                keyboard.id = alphabet[k];
+                keyboard.innerHTML = alphabet[k];
 
+                // Add click event handler to each button
+                keyboard.onclick = function () {
+                    if (x <= 5) {
+                    for (let y = 0; y < answerData.length; y++) {
+                        console.log(keyboard.id)
+                        console.log(answerData[y])
+                            if (answerData[y].toLowerCase().includes(keyboard.id)) {
+                               let div = document.getElementById(y)
+                               div.innerHTML=answerData[y]
+                               keyboard.className = "correct"
+                            }
+                           
+                        }
+                        if(!answerData.includes(keyboard.id.toLowerCase())==true&&!answerData.includes(keyboard.id.toUpperCase())==true){
+                            keyboard.className = "faulse"
+                            x++;
+                            console.log(x)
+                        }
+                    } else {
+                        console.log("u won")
+                    }
+                   
+                };
 
-//4
-
-
+            }
+       
     })
-    .catch((error) => {
-        // Handle errors that may occur during the fetch operation
-        console.error('Error fetching data:', error);
-    });
