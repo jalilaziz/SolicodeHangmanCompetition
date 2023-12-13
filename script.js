42a8ef6e0a0b168163baa3b0e1245d4bc49d82cb
jalilaziz <jaliilaziiz@gmail.com>
2023-12-13 11:50:36 +0100

    code clean except final image

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// Fetch JSON data from 'hangman-game.json'
-fetch('hangman-game.json')
-    .then((response) => response.json()) // Parse the response as JSON
-    .then(async (json) => {
+function startAgain() {
+    // Fetch JSON data from 'hangman-game.json'
+    fetch('hangman-game.json')
+        .then((response) => response.json()) // Parse the response as JSON
+        .then(async (json) => {
 
             // Extract 'id' parameter from the URL query string using URLSearchParams
             const params = new URLSearchParams(window.location.search);
@@ -12,61 +13,61 @@ fetch('hangman-game.json')
 
             // Retrieve question and answer data for the selected category and question
             let questionData = json[id][i].question;
-            let answerData = json[id][i].answer.replaceAll(" ","").split('');
+            let answerData = json[id][i].answer.replaceAll(" ", "").split('');
 
             // Find the HTML elements to update with the question and answer data
             let question = document.getElementById("question");
             question.innerHTML = questionData;
 
             let answer = document.querySelector("#answers");
+            answer.innerHTML = "";
 
-        // Create div elements for each character in the answerData
-        for (let j = 0; j < answerData.length; j++) {
-            let div = document.createElement("div");
-            div.classList.add("answer_place");
-            div.setAttribute("id", j);
-            answer.appendChild(div);
-        }
+            // Create div elements for each character in the answerData
+            for (let j = 0; j < answerData.length; j++) {
+                let div = document.createElement("div");
+                div.classList.add("answer_place");
+                div.setAttribute("id", j);
+                answer.appendChild(div);
+            }
 
-        // Define the alphabet for keyboard buttons
-        let alphabet = "qwertyuiopasdfghjklzxcvbnm";
+            // Define the alphabet for keyboard buttons
+            let alphabet = "qwertyuiopasdfghjklzxcvbnm";
 
-        //for validing if u lose or won 
-        let x = 0;
-        let keyboardDiv = document.getElementById("button")
+            // For validating if you lose or win 
+            let x = 0;
+            let keyboardDiv = document.getElementById("button");
+            keyboardDiv.innerHTML = "";
 
-        // Create keyboard buttons dynamically
-        for (let k = 0; k < alphabet.length; k++) {
-            let keyboard = document.createElement("button");
-            keyboardDiv.appendChild(keyboard)
-            keyboard.classList = "letter"
-            keyboard.id = alphabet[k];
-            keyboard.innerHTML = alphabet[k];
+            // Create keyboard buttons dynamically
+            for (let k = 0; k < alphabet.length; k++) {
+                let keyboard = document.createElement("button");
+                keyboardDiv.appendChild(keyboard);
+                keyboard.id = alphabet[k];
+                keyboard.innerHTML = alphabet[k];
 
-            // Add click event handler to each button
-            keyboard.onclick = function () {
-                if (x <= 5) {
-                    for (let y = 0; y < answerData.length; y++) {
-                        console.log(keyboard.id)
-                        console.log(answerData[y])
-                        if (answerData[y].toLowerCase().includes(keyboard.id)) {
-                            let div = document.getElementById(y)
-                            div.innerHTML = answerData[y]
-                            keyboard.className = "correct"
+                // Add click event handler to each button
+                keyboard.onclick = function () {
+                    if (x <= 5) {
+                        for (let y = 0; y < answerData.length; y++) {
+                            // Check if the clicked letter is in the answer
+                            if (answerData[y].toLowerCase().includes(keyboard.id)) {
+                                let div = document.getElementById(y)
+                                div.innerHTML = answerData[y];
+                                keyboard.style.backgroundColor = "#0A7308"; // Set background color to green for correct guesses
+                                keyboard.style.color = "white";
+                            }
                         }
-
+                        // Check if the clicked letter is not in the answer
+                        if (!answerData.includes(keyboard.id.toLowerCase()) && !answerData.includes(keyboard.id.toUpperCase())) {
+                            x++;
+                            keyboard.style.backgroundColor = "red"; // Set background color to red for incorrect guesses
+                            keyboard.style.color = "white"
+                        }
+                    } else {
+                        console.log("You won!")
                     }
-                    if (!answerData.includes(keyboard.id.toLowerCase()) == true && !answerData.includes(keyboard.id.toUpperCase()) == true) {
-                        keyboard.className = "faulse"
-                        x++;
-                        console.log(x)
-                    }
-                } else {
-                    console.log("u won")
-                }
-
-            };
-
-        }
-
-    })
+                };
+            }
+        });
+}
+startAgain();
