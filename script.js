11d6115030e7af5910680093b24c36ae7ea822e0
Fahd <suirita.fahd.solicode.gmail.com>
2023-12-13 10:12:05 +0100

    correcting mistakes

--- script.js
+++ script.js
@@ -2,17 +2,6 @@
 fetch('hangman-game.json')
     .then((response) => response.json()) // Parse the response as JSON
     .then(async (json) => {
-<<<<<<< HEAD
-        console.log(json)
-        // Extract 'id' parameter from the URL query string using URLSearchParams
-        const params = new URLSearchParams(window.location.search);
-        const id = params.get('id');
-
-        // Generate a random index 'i' to select a question from the specified category
-        let i = Math.floor(Math.random() * 6);
-        console.log(json[id][i].question)
-=======
->>>>>>> d650ed47957405e5353c07b7f26b7e185a3d1d87
 
         // Extract 'id' parameter from the URL query string using URLSearchParams
         const params = new URLSearchParams(window.location.search);
@@ -29,7 +18,7 @@ fetch('hangman-game.json')
         let question = document.getElementById("question");
         question.innerHTML = questionData;
 
-        let answer = document.querySelector(".answers");
+        let answer = document.querySelector("#answers");
 
         // Create div elements for each character in the answerData
         for (let j = 0; j < answerData.length; j++) {
@@ -42,15 +31,14 @@ fetch('hangman-game.json')
         // Define the alphabet for keyboard buttons
         let alphabet = "qwertyuiopasdfghjklzxcvbnm";
 
-        //for validing if u lose or won 
+        // For validating if you lose or win 
         let x = 0;
         let keyboardDiv = document.getElementById("button")
 
         // Create keyboard buttons dynamically
         for (let k = 0; k < alphabet.length; k++) {
             let keyboard = document.createElement("button");
-            keyboardDiv.appendChild(keyboard)
-            keyboard.classList = "letter"
+            keyboardDiv.appendChild(keyboard);
             keyboard.id = alphabet[k];
             keyboard.innerHTML = alphabet[k];
 
@@ -58,26 +46,22 @@ fetch('hangman-game.json')
             keyboard.onclick = function () {
                 if (x <= 5) {
                     for (let y = 0; y < answerData.length; y++) {
-                        console.log(keyboard.id)
-                        console.log(answerData[y])
+                        // Check if the clicked letter is in the answer
                         if (answerData[y].toLowerCase().includes(keyboard.id)) {
                             let div = document.getElementById(y)
                             div.innerHTML = answerData[y]
-                            keyboard.id.classList
+                            keyboard.style.backgroundColor = "#0A7308"; // Set background color to green for correct guesses
                         }
-
                     }
-                    if (!answerData.includes(keyboard.id.toLowerCase()) == true && !answerData.includes(keyboard.id.toUpperCase()) == true) {
-                        keyboard.id.className = "faulse"
+                    // Check if the clicked letter is not in the answer
+                    if (!answerData.includes(keyboard.id.toLowerCase()) && !answerData.includes(keyboard.id.toUpperCase())) {
                         x++;
                         console.log(x)
+                        keyboard.style.backgroundColor = "red"; // Set background color to red for incorrect guesses
                     }
                 } else {
-                    console.log("u won")
+                    console.log("You won!")
                 }
-
             };
-
         }
-
     })
